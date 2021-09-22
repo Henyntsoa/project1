9d89ceda28f1352090fc0fb86d759d2a6443a5c6
Henyntsoa Joda <rakotohenyntsoa@gmail.com>
2021-09-22 09:32:16 +0300

    Premier changement

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<nav class="nav-wrapper teal">
+   <div class="container">
+    <a href="#" class="brand-logo">BIENVENUE</a>
+    <a href="#" class="sidenav-trigger" data-target="mobile-links">Menu</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="#">Acceuil</a></li>
+      <li><a href="#">Mode de saisie</a></li>
+      <li><a href="#">Couleur</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="container">
+  <h4>Veuillez saisir vos nom et prénoms:</h4>
+  <br>
+  <div class="input-field">
+    <input type="text" id="nomATaper">
+    <label for="nomATaper" class="active">Nom</label>
+  </div>
+  <div class="input-field">
+    <input type="text" id="prenomATaper">
+    <label for="prenomATaper" class="active">Prénoms</label>
+  </div>
+  <button class="btn" @click="confirmation">Confirmer</button>
+  <h4>Bienvenue sur Vue, {{info}}.</h4>
+  
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      nomPersonne: '',
+      prenomPersonne: '',
+      info: ''
+    }
+  },
+  methods: {
+    confirmation() {
+      this.info = nomATaper.value + " " + prenomATaper.value
+    }
   }
+  // setup() {
+  //   return {nomPersonne, prenomPersonne, info}
+  // }
 }
 </script>
 
 <style>
+  /*@import '../src/components/materialize.min.css'
+/*    <link type="text/css" rel="stylesheet" href="../src/components/materialize.css"  media="screen,projection"/>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +58,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
